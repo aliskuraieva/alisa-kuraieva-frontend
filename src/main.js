f26964a1ed26bc4c32dd59b685b246a833ec362a
Alisa Kuraieva <alisa.mogila@gmail.com>
2025-05-07 18:37:28 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -37,11 +37,10 @@ const translations = {
     portfolio: "Portfolio",
     contacts: "Contacts",
     "about-title": "About me",
-    "about-text": `I'm a motivated and detail-oriented frontend developer with a
+    "about-text": `              I'm a motivated and detail-oriented frontend developer with a
               strong foundation in HTML, CSS, and JavaScript. I enjoy creating
               responsive, accessible, and visually engaging interfaces using
               modern technologies like React.
-              <br />
 
               In addition to React, I work extensively with Angular and
               TypeScript, building scalable applications with tools like NgRx
@@ -49,14 +48,12 @@ const translations = {
               full-stack technologies such as NestJS, PostgreSQL, and Docker,
               which helps me understand backend processes and collaborate more
               effectively across teams.
-              <br />
 
               I care deeply about code clarity, performance, and user
               experience. I'm a quick learner, a responsible team player, and
               someone who thrives on continuous improvement. Whether it's
               crafting clean UI components or architecting efficient flows, I
               focus on delivering thoughtful and reliable digital solutions.
-              <br />
 
               My goal is to build meaningful web experiences that are both
               beautiful and robust.`,
@@ -76,16 +73,8 @@ const translations = {
     portfolio: "Портфоліо",
     contacts: "Контакти",
     "about-title": "Про мене",
-    "about-text": `Я мотивована та уважна до деталей фронтенд-розробниця із міцною основою в HTML, CSS та JavaScript. Мені подобається створювати адаптивні, доступні та візуально привабливі інтерфейси, використовуючи сучасні технології, такі як React.
-<br />
-
-Окрім React, я активно працюю з Angular та TypeScript, розробляючи масштабовані додатки за допомогою таких інструментів, як NgRx для керування станом. Я також маю практичний досвід роботи з повноцінними технологіями, такими як NestJS, PostgreSQL та Docker, що допомагає мені краще розуміти бекенд-процеси та ефективно співпрацювати в командах.
-<br />
-
-Мені важлива ясність коду, продуктивність та досвід користувача. Я швидко навчаюсь, відповідальний командний гравець і людина, яка прагне до постійного вдосконалення. Незалежно від того, чи це створення чистих UI компонентів або архітектура ефективних потоків, я орієнтуюсь на надання продуманих і надійних цифрових рішень.
-<br />
-
-Моя мета — створювати значущі веб-досвіди, які є водночас красивими та надійними.`,
+    "about-text":
+      "Я вмотивований і орієнтований на деталі фронтенд розробник із міцною основою HTML, CSS і JavaScript. Мені подобається створювати адаптивні, доступні та візуально привабливі інтерфейси за допомогою сучасних технологій, таких як React. Окрім React, я багато працюю з Angular і TypeScript, створюючи масштабовані програми за допомогою таких інструментів, як NgRx для управління станом. Я також маю практичний досвід роботи з технологіями повного стеку, такими як NestJS, PostgreSQL і Docker, які допомагають мені розуміти внутрішні процеси та ефективніше співпрацювати між командами. Мені дуже важлива чіткість коду, продуктивність і взаємодія з користувачем. Я швидко навчаюся, відповідальний командний гравець і той, хто прагне постійно вдосконалюватися. Незалежно від того, чи це створення чистих компонентів інтерфейсу користувача чи розробка ефективних потоків, я зосереджуюсь на продуманих і надійних цифрових рішеннях. Моя мета полягає в створенні значущого веб-досвіду, який буде одночасно красивим і надійним.",
     "technologies-title": "Технології",
     "technologies-text":
       "Мій стек технологій включає сучасні інструменти та фреймворки для створення функціональних вебдодатків.",
@@ -103,10 +92,10 @@ const switchLanguage = (lang) => {
     }
   });
 
-  const aboutText = document.querySelector(".about-text");
-  if (aboutText) {
-    aboutText.innerHTML = translations[lang]["about-text"];
-  }
+  // const aboutText = document.querySelector(".about-text");
+  // if (aboutText) {
+  //   aboutText.innerHTML = translations[lang]["about-text"];
+  // }
 
   const technologiesText = document.querySelector(
     ".technologies-container > p"
